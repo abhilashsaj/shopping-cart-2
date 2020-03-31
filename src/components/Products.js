4188814999eb97ac38a1aad8291b7cd0a140038a
Abhilash Saj <abhilashsaj@gmail.com>
2020-03-31 09:29:28 +0530

    completed productsList component

--- src/components/Products.js
+++ src/components/Products.js
@@ -3,16 +3,19 @@ import React, { Component } from 'react'
 export default class Products extends Component {
     render() {
         const productItems = this.props.products.map( product => (
-                <div className="col-md-4">
+                <div className="col-md-4" key={product.id}>
                     <div className="thumbnail text-center">
-                        <a href={'#${product.id}'} onClick={this.props.handleAddToCart}>
-                            <img src={'/products/${product.sku}_2.jpg'} alt={product.title}/>
+                        <a href={'#'+product.id} onClick={(e) => this.props.handleAddToCart(e, product)}>
+                            <img src={'/products/'+product.sku+'_2.jpg'} alt={product.title}/>
                             <p>
                                 {product.title}
                             </p>
                         </a>
                         <b>
-                            {product.price}
+                            <p>
+                            ${product.price}
+                            </p>
+                            <button className="btn btn-primary" onClick={(e) => this.props.handleAddToCart(e, product)}>Add To Cart</button>
                         </b>
                     </div>
                 </div>
