686d8a449f9db3f4a4d12a20031c8549a6615144
Abhilash Saj <abhilashsaj@gmail.com>
2020-03-31 08:45:14 +0530

    connected json server and react app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { Component } from 'react'
 
-export default App;
+import Products  from "./components/Products";
+
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {products: [], filteredProducts: []}
+  }
+  componentWillMount(){
+    fetch("http://localhost:8000/products").then(res => res.json())
+    .then(data => this.setState({
+      products: data,
+      filteredProducts:data
+    }));
+  }
+  render() {
+    return (
+        <div className="container">
+          <h1>Ècommerce shopping cart application</h1>
+          <hr/>
+          <div className="row">
+            <div className="col-md-8">
+
+            </div>
+            <div className="col-md-4">
+              <Products products={this.state.filteredProducts} handleAddToCart={this.handleAddToCart}/>
+            </div>
+          </div>
+        </div>
+    )
+  }
+}
