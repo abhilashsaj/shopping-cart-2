0f2b9abad23b1891ba68b866619da682797badae
Abhilash Saj <abhilashsaj@gmail.com>
2020-04-01 11:47:13 +0530

    added filter by price

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -3,8 +3,30 @@ import React, { Component } from 'react'
 export default class Filter extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="row">
+                <div className="col-md-4">
+                    {this.props.count} product found
+                </div>
+                <div className="col-md-4">
+                    <label htmlFor="Order">
+                        Order By`
+                        <select className="form-control" value={this.props.sort} onChange={this.props.handleChangeSort}>
+                            <option value="">Select</option>
+                            <option value="lowest">Lowest to highest</option>
+                            <option value="highest">Highest to lowest</option>
+                        </select>
+                    </label>
+                </div>
+                <div className="col-md-4">
+                    <label htmlFor="Order">
+                        Order By`
+                        <select className="form-control" value={this.props.sort} onChange={this.props.handleChangeSort}>
+                            <option value="">Select</option>
+                            <option value="lowest">Lowest to highest</option>
+                            <option value="highest">Highest to lowest</option>
+                        </select>
+                    </label>
+                </div>
             </div>
         )
     }
